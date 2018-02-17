583788d398141a423c814a73c30cea16581fdef2
Immortalin <Immortalin@icloud.com>
2018-02-17 14:33:05 -0800

    One api works

--- backend/controllers/community.js
+++ backend/controllers/community.js
@@ -1,21 +1,43 @@
 Group = require('../models/Group')
+User = require('../models/User')
 
 
 exports.createCommunity = (req, res) => {
     console.log("Request Received!")
     console.log(req.body)
-    Group.create({
-        name: req.body.name,
-        description: req.body.description,
-        members: req.body.member_emails.map(email => User.findOne(email)),
-        admins: req.body.admin_emails.map(email => User.findOne(email)),
-        sponsors: req.body.sponsor_emails.map(email => User.findOne(email))
-    }, (err, group) => {
-        if (err) {
-            return res.status(500).send("Community Creation Failure - Internal Server Error");
-        }
+    return Promise.all(
+        [
+            Promise.all(
+                req.body.member_emails.map(email => User.findOne({ 'email': email }, { '_id': 1 }))
+            ),
+            Promise.all(
+                req.body.admin_emails.map(email => User.findOne({ 'email': email }, { '_id': 1 }))
+            ),
+            Promise.all(
+                req.body.sponsor_emails.map(email => User.findOne({ 'email': email }, { '_id': 1 }))
+            )
+        ]
+    ).then(
+        (result) => {
+            console.log(result)
+            let members = result[0];
+            let admins = result[1];
+            let sponsors = result[2];
+            Group.create({
+                name: req.body.name,
+                description: req.body.description,
+                members: members ? members.map(x => x._id) : [],
+                admins: admins.map(x => x._id),
+                sponsors: sponsors.map(x => x._id)
+            } , (err, group) => {
+                        if (err) {
+                            console.log(err)
+                            return res.status(500).send("Community Creation Failure - Internal Server Error");
+                        }
+                        res.status(200).send(group)
 
-        res.status(200).send(group)
-    })
-
+                    }
+                );
+            }
+            )
 }
